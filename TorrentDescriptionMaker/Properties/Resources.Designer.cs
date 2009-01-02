f398fe5285a2f6d7e5bf111bef00a76b54eeed6a
McoreD <McoreD@5297fa70-d7d5-11dd-86d7-074df07e0730>
2009-01-02 06:42:33 +0000

    Namespace renamed to TDMaker

--- TorrentDescriptionMaker/Properties/Resources.Designer.cs
+++ TorrentDescriptionMaker/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TorrentDescriptionMaker.Properties {
+namespace TDMaker.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TorrentDescriptionMaker.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TorrentDescriptionMaker.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TDMaker.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
