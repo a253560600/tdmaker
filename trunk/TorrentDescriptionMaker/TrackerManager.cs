d2226ee7353e8ace1908bb27e5a924b26f2d8c31
McoreD <McoreD@5297fa70-d7d5-11dd-86d7-074df07e0730>
2009-01-09 11:34:11 +0000

    1.2.00.0 Added support for creating Torrents using MonoTorrent

---
+++ trunk/TorrentDescriptionMaker/TrackerManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Runtime.Serialization;
+
+namespace TDMaker
+{
+    [Serializable]
+   public class TrackerManager
+    {
+
+        public List<Tracker> Trackers { get; set; }
+
+        public TrackerManager()
+        {
+            Trackers = new List<Tracker>();
+        }
+
+        public List<Tracker> Read()
+        {
+            if (File.Exists("trackers.xml"))
+            {
+
+                object obj = new object();
+                try
+                {
+                    using (FileStream fs = new FileStream("trackers.xml", FileMode.Open, FileAccess.Read))
+                    {
+
+                        XmlSerializer xs = new XmlSerializer(Trackers.GetType());
+                        obj = xs.Deserialize(fs);
+                        fs.Close();
+
+                    }
+
+                    if (obj != null)
+                    {
+                        Trackers = (List<Tracker>)obj;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                    throw;
+                }
+
+                // Declare the hashtable reference.
+
+
+                //// Open the file containing the data that you want to deserialize.
+                //FileStream fs = new FileStream("trackers.xml", FileMode.Open);
+                //try
+                //{
+                //    SoapFormatter formatter = new SoapFormatter();
+
+                //    // Deserialize the hashtable from the file and 
+                //    // assign the reference to the local variable.
+                //    Trackers = (List<Tracker>)formatter.Deserialize(fs);
+                //}
+                //catch (SerializationException e)
+                //{
+                //    Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                //}
+
+                //finally
+                //{
+                //    fs.Close();
+                //}
+
+            }
+
+            return Trackers;
+        }
+
+        public void Write(List<Tracker> tr)
+        {
+            try
+            {
+                if (tr.Count > 0)
+                {
+                    using (FileStream fs = new FileStream("trackers.xml", FileMode.Create))
+                    {
+                        XmlSerializer xs = new XmlSerializer(tr.GetType());
+                        xs.Serialize(fs, tr);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+               // fs.Close();
+            }
+            
+
+            //FileStream fs = new FileStream("trackers.xml", FileMode.Create);
+
+            //// Construct a SoapFormatter and use it 
+            //// to serialize the data to the stream.
+            //SoapFormatter formatter = new SoapFormatter();
+            //try
+            //{
+            //    formatter.Serialize(fs, Trackers);
+            //}
+            //catch (SerializationException e)
+            //{
+            //    Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+            //    throw;
+            //}
+            //finally
+            //{
+            //    fs.Close();
+            //}
+            
+        }
+
+    }
+}
---
+++ trunk/TorrentDescriptionMaker/TrackerManager2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TDMaker
+{
+    class TrackerManager2
+    {
+        public DataSet Trackers { get; set; }
+
+        public TrackerManager2()
+        {
+            Trackers = new DataSet("Settings");
+        }
+
+        public DataSet Read()
+        {
+            if (File.Exists("trackers.xml"))
+            {
+                Trackers.ReadXml("trackers.xml");
+            }
+
+            else
+            {
+                Trackers = getDataSet(); 
+            }
+
+            return Trackers;
+        }
+
+        private DataSet getDataSet()
+        {
+            DataSet ds = new DataSet("Settings");
+            DataTable dt = new DataTable("Trackers");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("AnnounceURL");
+            ds.Tables.Add(dt);
+
+            return ds;
+        }
+
+        public void Write(DataGridView dgv)
+        {
+            DataSet ds = getDataSet();
+            DataTable  dt = ds.Tables[0];
+
+            for (int i = 0; i < dgv.Rows.Count-1; i++)
+            {
+                DataRow row;
+                row = dt.NewRow();
+                row[0] = dgv.Rows[i].Cells[0].Value;
+                row[1] = dgv.Rows[i].Cells[1].Value;
+                dt.Rows.Add(row);
+            }
+
+            ds.Tables.Add(dt);
+
+            ds.WriteXml("trackers.xml");
+        }
+    }
+}
