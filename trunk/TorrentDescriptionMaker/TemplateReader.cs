69d8db4f418488d78754998d7c7c68a6f755625a
McoreD <McoreD@5297fa70-d7d5-11dd-86d7-074df07e0730>
2009-01-12 09:07:32 +0000

    Updated TODO: Support Templates next

---
+++ trunk/TorrentDescriptionMaker/TemplateReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TorrentDescriptionMaker;
+
+namespace TDMaker
+{
+    /// <summary>
+    /// Class responsible for reading Template Directories. 
+    /// A Template Directory has 5 files: 
+    /// GeneralInfo.txt; VideoInfo.txt; AudioInfo.txt; Disc.txt; File.txt
+    /// </summary>
+    public class TemplateReader
+    {
+        /// <summary>
+        /// Constructure of TemplateReader
+        /// </summary>
+        /// <param name="loc">Directory Path of the Template</param>
+        /// <param name="ti">TorrentInfo object that has MediaInfo</param>
+        public TemplateReader(string loc, TorrentInfo ti)
+        {
+            this.Location = loc;
+            this.TorrentInfo = ti;
+        }
+
+        /// <summary>
+        /// Location of the Template
+        /// </summary>
+        public string Location { get; private set; }
+        public TorrentInfo TorrentInfo { get; private set; }
+
+    }
+}
