62b222e79ef2b7f33c22015a94c0973d016c1d2f
mcored <mcored@5297fa70-d7d5-11dd-86d7-074df07e0730>
2010-01-05 08:02:34 +0000

    changelog updates; full support for mtn commands

--- trunk/TDMakerLib/Global/Adapter.cs
+++ trunk/TDMakerLib/Global/Adapter.cs
@@ -20,26 +20,42 @@ namespace TDMakerLib
         {
         	// Create MTN Arg
 
-                // Fill Screenshot object : coded parameters in alphabetical order
+                // Fill Screenshot object : coded parameters in alphabetical order except for columns, rows and width
+                
                 StringBuilder sbMTNArgs = new StringBuilder();
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().P_QuitAfterDone)
-                {
-                    sbMTNArgs.Append("-P ");
-                }
+                sbMTNArgs.Append(string.Format("-c {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().c_Columns));
+                sbMTNArgs.Append(string.Format("-r {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().r_Rows));     
+                
                 if (Engine.mtnProfileMgr.GetMtnProfileActive().w_Width != XMLSettingsScreenshot.w_Width_default) {
-                	sbMTNArgs.Append(string.Format("-w{0} ", Engine.mtnProfileMgr.GetMtnProfileActive().w_Width));                	
-                }
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().h_MinHeight != XMLSettingsScreenshot.h_MinHeight_default)
-                {
-                    sbMTNArgs.Append(string.Format("-h{0} ", Engine.mtnProfileMgr.GetMtnProfileActive().h_MinHeight));
+                	sbMTNArgs.Append(string.Format("-w {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().w_Width));                	
                 }
                 
-                sbMTNArgs.Append(string.Format("-c{0} ", Engine.mtnProfileMgr.GetMtnProfileActive().c_Columns));
-                sbMTNArgs.Append(string.Format("-r{0} ", Engine.mtnProfileMgr.GetMtnProfileActive().r_Rows));
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().k_ColorBackground != XMLSettingsScreenshot.k_ColorBackground_default)
-                {
-                    sbMTNArgs.Append(string.Format("-k{0} ", Engine.mtnProfileMgr.GetMtnProfileActive().k_ColorBackground));
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().a_AspectRatio != XMLSettingsScreenshot.a_AspectRatio_default) {
+                	sbMTNArgs.Append(string.Format("-a {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().a_AspectRatioValue));                	
                 }
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().b_SkipBlank != XMLSettingsScreenshot.b_SkipBlank_default) {
+                	sbMTNArgs.Append(string.Format("-b {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().b_SkipBlank));                	                	
+                }
+
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().B_OmitBegin != XMLSettingsScreenshot.B_OmitBegin_default)
+                {
+                    sbMTNArgs.Append(string.Format("-B {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().B_OmitBegin));
+                }
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().C_CutMovie != XMLSettingsScreenshot.C_CutMovie_default) {
+                    sbMTNArgs.Append(string.Format("-C {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().C_CutMovie));                	
+                }
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().D_EdgeDetection != XMLSettingsScreenshot.D_EdgeDetection_default)
+                {
+                    sbMTNArgs.Append(string.Format("-D {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().D_EdgeDetection));
+                }
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().E_OmitEnd != XMLSettingsScreenshot.E_OmitEnd_default)
+                {
+                    sbMTNArgs.Append(string.Format("-E {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().E_OmitEnd));
+                }                
 
                 if (Engine.mtnProfileMgr.GetMtnProfileActive().i_InfoOff || Engine.IsUNIX)
                 {
@@ -56,6 +72,32 @@ namespace TDMakerLib
                         sbMTNArgs.Append(string.Format("-F {0}:{1} ", Engine.mtnProfileMgr.GetMtnProfileActive().F_FontColor, Engine.mtnProfileMgr.GetMtnProfileActive().F_FontSize));
                     }
                 }
+
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().g_GapBetweenShots != XMLSettingsScreenshot.g_GapBetweenShots_default)
+                {
+                    sbMTNArgs.Append(string.Format("-g {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().g_GapBetweenShots));
+                }
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().h_MinHeight != XMLSettingsScreenshot.h_MinHeight_default)
+                {
+                    sbMTNArgs.Append(string.Format("-h {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().h_MinHeight));
+                }             
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().I_IndivScreens)
+                {
+                    sbMTNArgs.Append("-I ");
+                }
+               
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().j_JpgQuality != XMLSettingsScreenshot.g_GapBetweenShots_default)
+                {
+                    sbMTNArgs.Append(string.Format("-j {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().j_JpgQuality));
+                }
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().k_ColorBackground != XMLSettingsScreenshot.k_ColorBackground_default)
+                {
+                    sbMTNArgs.Append(string.Format("-k {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().k_ColorBackground));
+                }   
+                
                 if (!Engine.mtnProfileMgr.GetMtnProfileActive().t_TimeStampOff || Engine.IsUNIX)
                 {
                     sbMTNArgs.Append("-t ");
@@ -66,61 +108,28 @@ namespace TDMakerLib
                     {
                         sbMTNArgs.Append(string.Format("-L {0}:{1} ", Engine.mtnProfileMgr.GetMtnProfileActive().L_LocInfo, Engine.mtnProfileMgr.GetMtnProfileActive().L_LocTimestamp));
                     }
-                }
-
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().g_GapBetweenShots != XMLSettingsScreenshot.g_GapBetweenShots_default)
-                {
-                    sbMTNArgs.Append(string.Format("-g {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().g_GapBetweenShots));
-                }
+                }   
                 
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().j_JpgQuality != XMLSettingsScreenshot.g_GapBetweenShots_default)
-                {
-                    sbMTNArgs.Append(string.Format("-j {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().j_JpgQuality));
-                }
-                
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().s_TimeStep != XMLSettingsScreenshot.s_TimeStep_default)
-                {
-                    sbMTNArgs.Append(string.Format("-s {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().s_TimeStep));
-                }
-                
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().D_EdgeDetection != XMLSettingsScreenshot.D_EdgeDetection_default)
-                {
-                    sbMTNArgs.Append(string.Format("-D {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().D_EdgeDetection));
-                }
-
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().B_OmitBegin != XMLSettingsScreenshot.B_OmitBegin_default)
-                {
-                    sbMTNArgs.Append(string.Format("-B {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().B_OmitBegin));
-                }
-
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().E_OmitEnd != XMLSettingsScreenshot.E_OmitEnd_default)
-                {
-                    sbMTNArgs.Append(string.Format("-E {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().E_OmitEnd));
-                }
-
-                // Not supported in MTN 2.45
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().z_AlwaysSeek)
-                {
-                    sbMTNArgs.Append("-z ");
-                }
-                else if (Engine.mtnProfileMgr.GetMtnProfileActive().Z_AlwaysNonSeek)
-                {
-                    sbMTNArgs.Append("-Z ");
-                }
-
                 if (Engine.mtnProfileMgr.GetMtnProfileActive().N_InfoSuffix != XMLSettingsScreenshot.N_InfoSuffix_default || Engine.IsUNIX)
                 {
                     sbMTNArgs.Append(string.Format("-N {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().N_InfoSuffix));                    
                 }
-
+                
                 if (Engine.mtnProfileMgr.GetMtnProfileActive().o_OutputSuffix != XMLSettingsScreenshot.o_OutputSuffix_default)
                 {
                     sbMTNArgs.Append(string.Format("-o {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().o_OutputSuffix));
-                }
+                }      
                 
-                if (Engine.mtnProfileMgr.GetMtnProfileActive().v_Verbose)
+                sbMTNArgs.Append(string.Format("-O \"{0}\" ", Engine.GetScreenShotsDir()));     
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().P_QuitAfterDone)
                 {
-                    sbMTNArgs.Append("-v ");
+                    sbMTNArgs.Append("-P ");
+                }                
+
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().s_TimeStep != XMLSettingsScreenshot.s_TimeStep_default)
+                {
+                    sbMTNArgs.Append(string.Format("-s {0} ", Engine.mtnProfileMgr.GetMtnProfileActive().s_TimeStep));
                 }
                 
                 if (Engine.mtnProfileMgr.GetMtnProfileActive().T_TitleTextAdd)
@@ -129,8 +138,22 @@ namespace TDMakerLib
                     {
 	                    sbMTNArgs.Append(string.Format("-T \"{0}\" ", Engine.conf.txtMTN_T_Title));                        
                     }
+                }    
+                                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().v_Verbose)
+                {
+                    sbMTNArgs.Append("-v ");
                 }
-                sbMTNArgs.Append(string.Format("-O \"{0}\" ", Engine.GetScreenShotsDir()));
+                
+                if (Engine.mtnProfileMgr.GetMtnProfileActive().z_AlwaysSeek)
+                {
+                    sbMTNArgs.Append("-z ");
+                }
+                else if (Engine.mtnProfileMgr.GetMtnProfileActive().Z_AlwaysNonSeek)
+                {
+                    sbMTNArgs.Append("-Z ");
+                }
+  
         	return sbMTNArgs.ToString();
         }	
 	}
