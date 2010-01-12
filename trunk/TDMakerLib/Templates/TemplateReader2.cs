c8bf6978c0716716d465e47dd661e88c84faccd5
flexy123 <flexy123@5297fa70-d7d5-11dd-86d7-074df07e0730>
2010-01-12 12:50:39 +0000

    Added TemplateItem class for TemplateReader2

--- trunk/TDMakerLib/Templates/TemplateReader2.cs
+++ trunk/TDMakerLib/Templates/TemplateReader2.cs
@@ -5,40 +5,40 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections.Specialized;
 using System.Globalization;
+using TDMakerLib.Templates;
 
 namespace TDMakerLib
 {
     public class TemplateReader2
     {
+        List<TemplateItem> Items { get; set; }
+
         public TemplateReader2(string summary)
         {
-            string[] lines = Regex.Split(summary, "\r\n");
-
-            List<string> listSyntax = new List<string>();
-            List<string> listValues = new List<string>();
-            string prefix = string.Empty;
-            foreach (string line in lines)
+            if (!string.IsNullOrEmpty(summary))
             {
-                string[] temp = Regex.Split(line, " : ");
-                if (temp.Length > 1)
+                Items = new List<TemplateItem>();
+                string prefix = string.Empty;
+                string[] lines = summary.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string line in lines)
                 {
-                    TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
-                    string syntax = myTI.ToTitleCase(temp[0].Trim().Replace(@"\", "").Replace("/", "").Replace("(", "").Replace(")", "").Replace("*", "")).Replace(" ", "");
-                    string value = temp[1].Trim();
-                    listSyntax.Add(string.Format("%{0}_{1}%", prefix, syntax));
-                    listValues.Add(value);
-                }
-                else if (temp.Length == 1)
-                {
-                    prefix = temp[0].Trim();
+                    string[] temp = line.Split(new[] { " : " }, StringSplitOptions.None);
+
+                    if (temp.Length == 2 && !string.IsNullOrEmpty(prefix))
+                    {
+                        Items.Add(new TemplateItem(temp[0], temp[1], prefix));
+                    }
+                    else if (temp.Length == 1)
+                    {
+                        prefix = temp[0].Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format("Error TemplateReader2.cs: {0}, {1}", line, prefix));
+                    }
                 }
             }
-
-            for (int i = 0; i < listSyntax.Count; i++)
-            {
-                System.Diagnostics.Debug.WriteLine(listSyntax[i] + " = " + listValues[i]);
-            }
-
         }
     }
 }
